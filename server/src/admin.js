a18586193cd065308d7867587a99d373efedf150
Lukas <lukas.baronyai@gmail.com>
2020-03-29 23:26:31 +0200

    move user API behind admin security

--- server/src/admin.js
+++ server/src/admin.js
@@ -50,6 +50,53 @@ function createRouter(db) {
     );
   });
 
+  router.get('/api/admin/workout/user/all', function (req, res, next) {
+    db.query('SELECT workout.username,name,sum(amount) ' +
+      'FROM workout ' +
+      'NATURAL JOIN university ' +
+      'NATURAL JOIN workout_data ' +
+      'GROUP BY username,name ' +
+      'ORDER BY SUM(amount) DESC ' +
+      'LIMIT $1;',
+      [
+        req.query.limit
+      ],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.status(200).json(results.rows);
+        }
+      }
+    );
+  });
+
+  router.get('/api/admin/workout/user/today', function (req, res, next) {
+    db.query('SELECT workout.username,name,sum(amount) ' +
+      'FROM workout ' +
+      'NATURAL JOIN university ' +
+      'NATURAL JOIN workout_data ' +
+      'WHERE date = current_date::date ' +
+      'GROUP BY username,name ' +
+      'ORDER BY SUM(amount) DESC ' +
+      'LIMIT $1;',
+      [
+        req.query.limit
+      ],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.header("Content-Type", "application/json; charset=utf-8");
+          res.status(200).json(results.rows);
+        }
+      }
+    );
+  });
+
   router.delete('/api/admin/workout/:id', (req, res, next) => {
     db.query(
       'DELETE FROM WORKOUT WHERE workoutId=$1;',
